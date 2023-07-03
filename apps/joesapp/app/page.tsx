05c6f2c2ba5d79a7e2511dbca7a5919bdac8da38
Joe Denny <josephpauldenny@gmail.com>
2023-07-03 17:37:54 +0100

    first commit

---
+++ apps/joesapp/app/page.tsx
@@ -0,0 +1,16 @@
+import styles from "./page.module.css";
+
+// Import the shared function into the Next.js application
+import { getSiteInfo } from "../../../packages/site-info";
+
+export default function Home() {
+  let siteInfo = getSiteInfo(); //Define a variable to get the values
+
+  return (
+    <main className={styles.main}>
+      <h1>Joe&apos;s App</h1>
+      <h2 className={styles.title}>Welcome to {siteInfo.title}</h2>
+      <p className={styles.description}>{siteInfo.subtitle}</p>
+    </main>
+  );
+}
